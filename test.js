47fa79b2f301ece22c8305b3154566cf5d7f2a7c
ryan anzer <ryan.anzer@epitech.eu>
2019-09-22 02:21:06 +0200

    🚧 dump

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ hash('./index.js').then((res) => {
 	}
 	return hash('./.github', {max: 5});
 }).then((res) => {
-	assert.equal(res, '9d54a120e944e07fbc8924581be5b68d35762a591672e0a34bdef7b8fb5576c5');
+	assert.equal(res, 'cc5c36cda8b118de227ddc7cd9ba38b472686a8e52b7a0bdc1ac82ff91a350a5');
 }).catch((err) => {
 	console.log(err);
 	process.exit(1);
