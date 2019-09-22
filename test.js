c90f8b73e0fa6256882540470b9f1bf366e1540d
ryan anzer <ryan.anzer@epitech.eu>
2019-09-22 02:13:44 +0200

    🚧 dump

--- test.js
+++ test.js
@@ -4,12 +4,15 @@ const hash = require('./index.js'),
 
 hash('./index.js').then((res) => {
 	assert.equal(res, '06dea52826eb8e810458ce7fe3f2cfbefffd6b6022350e711dad8456d53fc12f');
-}).catch((err) => {
-	console.log(err);
-	process.exit(1);
-});
-
-hash('./.github', {max: 5}).then((res) => {
+	return hash('./.github', {raw: true, max: 5});
+}).then((res) => {
+	for (let i in res) {
+		assert.equal(typeof res[i], 'string');
+		assert.equal(typeof i, 'string');
+		console.log(i, res[i]);
+	}
+	return hash('./.github', {max: 5});
+}).then((res) => {
 	assert.equal(res, '9d54a120e944e07fbc8924581be5b68d35762a591672e0a34bdef7b8fb5576c5');
 }).catch((err) => {
 	console.log(err);
